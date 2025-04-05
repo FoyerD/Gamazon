6ac3d3ae69c0988666f9bb802722cc0716454264
almog <almogzh@post.bgu.ac.il>
2025-04-05 14:06:40 +0300

    Add Response and ResponseT classes for handling application responses

---
+++ src/main/java/Application/Responses/Response.java
@@ -0,0 +1,19 @@
+package Application.Responses;
+
+public class Response {
+    private String errorMessage;
+
+    public Response(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public Response() { }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+    
+    public boolean errorOccurred() { 
+        return errorMessage != null; 
+    }
+}
