3d11ab64d553da3d02c5724885074b6301fb1b29
Tomy <yachil@post.bgu.ac.il>
2025-04-06 03:01:00 +0300

    added IShoppingCartRepository and IShoppingBasketRepository

---
+++ src/main/java/Domain/Shopping/IShoppingBasketRepository.java
@@ -0,0 +1,7 @@
+package Domain.Shopping;
+
+public interface IShoppingBasketRepository {
+    public ShoppingBasket getById(int id); 
+    public void add(ShoppingBasket shoppingBasket);
+    public void remove(ShoppingBasket shoppingBasket);
+}
