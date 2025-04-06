05e21dddb81f1165e0a3ba87009f519622eb45ce
almog <almogzh@post.bgu.ac.il>
2025-04-06 08:12:32 +0300

    Remove IUser class and update IShoppingCartRepository formatting

--- src/main/java/Domain/Shopping/IShoppingCartRepository.java
+++ src/main/java/Domain/Shopping/IShoppingCartRepository.java
@@ -1,7 +1,10 @@
 package Domain.Shopping;
 
 public interface IShoppingCartRepository {
+
     public ShoppingCart getById(int id); 
+
     public void add(ShoppingCart shoppingCart);
+    
     public void remove(ShoppingCart shoppingCart);
 }
