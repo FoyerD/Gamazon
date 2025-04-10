a3d5cc1cdfe9595f67a4e7da4b3b7e8f3dd9c459
roioha3 <roioha@post.bgu.ac.il>
2025-04-10 19:47:13 +0300

    Implemented most of Product

--- src/main/java/Domain/Store/Product.java
+++ src/main/java/Domain/Store/Product.java
@@ -1,6 +1,45 @@
 package Domain.Store;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+
 public class Product {
 
+    private String productId;
+    private String name;
+    private Set<Category> categories;
+
+    public class Category {}
+    public Product(String productId, String name, Set<Category> categories){
+        this.productId = productId;
+        this.name = name;
+        this.categories = categories;
+    }
     
+    public Product(String productId, String name){
+        this.productId = productId;
+        this.name = name;
+        this.categories = new LinkedHashSet<>();
+    }
+
+    public boolean addCategory(Category c){
+        return categories.add(c);
+    }
+    
+    public boolean removeCategory(Category c){
+        return categories.remove(c);
+    }
+    
+    public String getProductId(){
+        return productId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Set<Category> getCategories(){
+        return categories;
+    }
 }
