4ebfa3a58b4cd65e4227f03d67dbad946fc3a389
FoyerD <amitfoye@post.bgu.ac.il>
2025-04-15 19:31:54 +0300

    removed methods from store repository, will move to facade

--- src/main/java/Domain/Store/IStoreRepository.java
+++ src/main/java/Domain/Store/IStoreRepository.java
@@ -1,23 +1,7 @@
 package Domain.Store;
 
-import java.util.Map;
-
 import Domain.IRepository;
-import Domain.Shopping.IShoppingBasket;
-import Domain.Shopping.IShoppingCart;
 
 public interface IStoreRepository extends IRepository<Store, String> {
-
-    //void checkProductsExist(int storeId, Map<Integer, Item> products);
-
-    //double calculateBasketPrice(IShoppingBasket basket);
-
-    //void removeCartQuantity(IShoppingCart cart);
-
-    //void addCartQuantity(IShoppingCart cart);
-
-    //Store getStore(Object storeId);
-
-    //void closeStore(int storeId);
 
 }
