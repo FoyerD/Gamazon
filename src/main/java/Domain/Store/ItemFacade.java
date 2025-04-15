e7071fdadcf8174e383d4d782439af40f32aede5
roioha3 <roioha@post.bgu.ac.il>
2025-04-15 19:43:53 +0300

    Implemented Item, ItemFacade, ItemService, MemoryItemRepo

---
+++ src/main/java/Domain/Store/ItemFacade.java
@@ -0,0 +1,33 @@
+package Domain.Store;
+
+import java.util.List;
+
+public class ItemFacade {
+    private IItemRepository itemRepository;
+
+    public ItemFacade(IItemRepository itemRepository){
+        this.itemRepository = itemRepository;
+    }
+
+    public List<Item> filterItems(ItemFilter itemFilter){
+        List<Item> availableItems = itemRepository.getAvailabeItems();
+        return availableItems.stream().filter(itemFilter::matchesFilter).toList();
+    }
+
+    public List<Item> getItemsProductId(String productId){
+        return itemRepository.getByProductId(productId);
+    }
+
+    public Item getItem(String storeId, String productId){
+        return itemRepository.getItem(storeId, productId);
+    }
+
+    public List<Item> getItemsByStoreId(String storeId){
+        return itemRepository.getByStoreId(storeId);
+    }
+
+    
+    public List<Item> getAvailableItems(){
+        return itemRepository.getAvailabeItems();
+    }
+}
