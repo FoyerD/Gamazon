1c25ce0e21ef48bc1a5eafebb4a7bcdfb0358694
FoyerD <amitfoye@post.bgu.ac.il>
2025-04-15 14:32:02 +0300

    added tests to CustomerServiceService

--- src/test/java/StoreTests/StoreServiceTests.java
+++ src/test/java/StoreTests/StoreServiceTests.java
@@ -1,9 +1,39 @@
 package StoreTests;
 
 import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertFalse;
+import org.junit.Before;
 import org.junit.Test;
 
+import Application.Response;
+import Application.StoreService;
+
+
+
 public class StoreServiceTests {
-    
+
+    private StoreService storeService;
+
+    @Before
+    public void setUp() {
+        storeService = new StoreService();
+
+    }
+
+    @Test
+    public void GivenNewStore_WhenCreateStore_ThenReturnTrue() {
+        String sessionToken = "validSessionToken";
+        String storeName = "NewStore";
+        Response<Boolean> result = storeService.addStore(sessionToken, storeName, "A new store");
+        assertTrue(result.getValue());
+    }
+
+    @Test
+    public void GivenExistingStoreName_WhenOpenStore_ThenReturnFalse() {
+        String sessionToken = "validSessionToken";
+        String storeName = "ExistingStore";
+        storeService.addStore(sessionToken, storeName, "A new store");
+        Response<Boolean> result = storeService.addStore(sessionToken, storeName,"I should not exist");
+        assertFalse(result.getValue());
+    }
 }
