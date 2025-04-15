cd83d2d160ec6dcb9331754d34b0292ab49843b4
FoyerD <amitfoye@post.bgu.ac.il>
2025-04-15 15:03:02 +0300

    finished store tests

--- src/test/java/StoreTests/StoreServiceTests.java
+++ src/test/java/StoreTests/StoreServiceTests.java
@@ -5,18 +5,23 @@ import static org.junit.Assert.assertFalse;
 import org.junit.Before;
 import org.junit.Test;
 
+import Application.MarketService;
 import Application.Response;
 import Application.StoreService;
+import Domain.IMarketFacade;
+import Domain.User.IUserRepository;
 
 
 
 public class StoreServiceTests {
 
     private StoreService storeService;
+    private MarketService marketService;
 
     @Before
     public void setUp() {
         storeService = new StoreService();
+        marketService = null;
 
     }
 
@@ -34,6 +39,27 @@ public class StoreServiceTests {
         String storeName = "ExistingStore";
         storeService.addStore(sessionToken, storeName, "A new store");
         Response<Boolean> result = storeService.addStore(sessionToken, storeName,"I should not exist");
+        assertFalse(result.getValue());
+    }
+
+    @Test
+    public void GivenClosedStore_WhenOpenStore_ThenReturnTrue() {
+        String sessionToken = "validSessionToken";
+        String storeName = "ExistingStore";
+        Integer storeId = 1234;
+        storeService.addStore(sessionToken, storeName, "A new store");
+        marketService.closeStore(storeId);
+        Response<Boolean> result = storeService.openStore(sessionToken, storeId.toString());
+        assertTrue(result.getValue());
+    }
+
+    @Test
+    public void GivenOpenStore_WhenOpenStore_ThenReturnFalse() {
+        String sessionToken = "validSessionToken";
+        String storeName = "ExistingStore";
+        Integer storeId = 1234;
+        storeService.addStore(sessionToken, storeName, "A new store");
+        Response<Boolean> result = storeService.openStore(sessionToken, storeId.toString());
         assertFalse(result.getValue());
     }
 }
