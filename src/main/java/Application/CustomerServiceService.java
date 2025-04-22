288e6443fe2fae9d6664c852baf6a4b64a3379dd
FoyerD <amitfoye@post.bgu.ac.il>
2025-04-22 11:41:32 +0300

    added }

--- src/main/java/Application/CustomerServiceService.java
+++ src/main/java/Application/CustomerServiceService.java
@@ -18,6 +18,7 @@ public class CustomerServiceService {
 
     public boolean isInitialized() {
         return this.storeFacade != null;
+    }
 
     public Response<Boolean> addFeedback(String customerId, String storeId, String productId, String comment) {
         try {
