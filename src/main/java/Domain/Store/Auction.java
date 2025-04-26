173f5b774d5d012c46315d752973fae09db0072e
FoyerD <amitfoye@post.bgu.ac.il>
2025-04-26 23:17:14 +0300

    added auction to store facade

--- src/main/java/Domain/Store/Auction.java
+++ src/main/java/Domain/Store/Auction.java
@@ -1,38 +1,30 @@
 package Domain.Store;
 
-import java.sql.Date;
-import java.util.UUID;
+import java.util.Date;
 
-enum AuctionStatus {
-    ACTIVE,
-    INACTIVE,
-    COMPLETED
-}
 
 public class Auction {
-    UUID auctionId;
+    String auctionId;
     Date auctionStartDate;
     Date auctionEndDate;
     float startPrice;
     float currentPrice;
-    AuctionStatus auctionStatus;
-    UUID storeId;
-    UUID productId;
-    UUID currentBidderId;
+    String storeId;
+    String productId;
+    String currentBidderId;
 
 
-    public Auction(UUID auctionId, Date auctionStartDate,
-                   Date auctionEndDate, float startPrice, float currentPrice, AuctionStatus auctionStatus,
-                   UUID storeId, UUID productId, UUID currentBidderId) {
+    public Auction(String auctionId, Date auctionStartDate,
+                   Date auctionEndDate, float startPrice, float currentPrice,
+                   String storeId, String productId) {
         this.auctionId = auctionId;
         this.auctionStartDate = auctionStartDate;
         this.auctionEndDate = auctionEndDate;
         this.startPrice = startPrice;
         this.currentPrice = currentPrice;
-        this.auctionStatus = auctionStatus;
         this.storeId = storeId;
         this.productId = productId;
-        this.currentBidderId = currentBidderId;
+        this.currentBidderId = null;
     }
 
     public Auction() {
@@ -41,7 +33,6 @@ public class Auction {
         this.auctionEndDate = null;
         this.startPrice = -1;
         this.currentPrice = -1;
-        this.auctionStatus = null;
         this.storeId = null;
         this.productId = null;
         this.currentBidderId = null;
@@ -51,7 +42,7 @@ public class Auction {
         return auctionId.toString();
     }
     public void setAuctionId(String auctionId) {
-        this.auctionId = UUID.nameUUIDFromBytes(auctionId.toString().getBytes());
+        this.auctionId = auctionId.toString();
     }
     public Date getAuctionStartDate() {
         return auctionStartDate;
@@ -77,29 +68,23 @@ public class Auction {
     public void setCurrentPrice(float currentPrice) {
         this.currentPrice = currentPrice;
     }
-    public AuctionStatus getAuctionStatus() {
-        return auctionStatus;
-    }
-    public void setAuctionStatus(AuctionStatus auctionStatus) {
-        this.auctionStatus = auctionStatus;
-    }
     public String getStoreId() {
         return storeId.toString();
     }
     public void setStoreId(String storeId) {
-        this.storeId = UUID.nameUUIDFromBytes(storeId.toString().getBytes());
+        this.storeId = storeId.toString();
     }
     public String getProductId() {
         return productId.toString();
     }
     public void setProductId(String productId) {
-        this.productId = UUID.nameUUIDFromBytes(productId.toString().getBytes());
+        this.productId = productId;
     }
     public String getCurrentBidderId() {
         return currentBidderId.toString();
     }
     public void setCurrentBidderId(String currentBidderId) {
-        this.currentBidderId = UUID.nameUUIDFromBytes(currentBidderId.toString().getBytes());
+        this.currentBidderId = currentBidderId;
     }
     
 }
