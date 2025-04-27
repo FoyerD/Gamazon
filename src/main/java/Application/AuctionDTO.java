f04df4eb4f57c7d7711a259405caebbd3adaf111
FoyerD <amitfoye@post.bgu.ac.il>
2025-04-27 19:13:08 +0300

    finished auction dto

--- src/main/java/Application/AuctionDTO.java
+++ src/main/java/Application/AuctionDTO.java
@@ -1,5 +1,57 @@
 package Application;
 
+import java.util.Date;
+
 public class AuctionDTO {
-    
+    private String auctionId;
+    private Date auctionStartDate;
+    private Date auctionEndDate;
+    private float startPrice;
+    private float currentPrice;
+    private String storeId;
+    private String productId;
+    private String currentBidderId;
+
+    public AuctionDTO(String auctionId, Date auctionStartDate, Date auctionEndDate, float startPrice, float currentPrice, String storeId, String productId, String currentBidderId) {
+        this.auctionId = auctionId;
+        this.auctionStartDate = auctionStartDate;
+        this.auctionEndDate = auctionEndDate;
+        this.startPrice = startPrice;
+        this.currentPrice = currentPrice;
+        this.storeId = storeId;
+        this.productId = productId;
+        this.currentBidderId = currentBidderId;
+    }
+
+    public String getAuctionId() {
+        return auctionId;
+    }
+
+    public Date getAuctionStartDate() {
+        return auctionStartDate;
+    }
+
+    public Date getAuctionEndDate() {
+        return auctionEndDate;
+    }
+
+    public float getStartPrice() {
+        return startPrice;
+    }
+
+    public float getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public String getStoreId() {
+        return storeId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getCurrentBidderId() {
+        return currentBidderId;
+    }
 }
