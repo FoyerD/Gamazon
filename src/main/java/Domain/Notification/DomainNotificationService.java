1dae228af73dec730b4cc6ebd5e2e74c443e4212
roioha3 <roioha@post.bgu.ac.il>
2025-05-20 13:03:37 +0300

    added store of delayed notifications.

---
+++ src/main/java/Domain/Notification/DomainNotificationService.java
@@ -0,0 +1,25 @@
+package Domain.Notification;
+
+import org.springframework.stereotype.Component;
+
+import UI.webSocketConfigurations.AbstractNotificationService;
+import UI.webSocketConfigurations.ConnectedUserRegistry;
+import UI.webSocketConfigurations.WebSocketNotifier;
+
+@Component
+public class DomainNotificationService extends AbstractNotificationService {
+
+    private final INotificationRepository repo;
+
+    public DomainNotificationService(WebSocketNotifier notifier,
+                                     ConnectedUserRegistry registry,
+                                     INotificationRepository repo) {
+        super(notifier, registry);
+        this.repo = repo;
+    }
+
+    @Override
+    protected void storeUndelivered(String userId, String content) {
+        repo.addNotification(userId, content);
+    }
+}
