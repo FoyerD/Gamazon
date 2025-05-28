5a6d2e0edef48df00b29b24eb1aaba7e5399ee15
Aviad-Lampert <aviadlam@post.bgu.ac.il>
2025-05-29 00:30:20 +0300

    some commit

--- src/main/java/UI/presenters/TradingPresenter.java
+++ src/main/java/UI/presenters/TradingPresenter.java
@@ -31,25 +31,26 @@ public class TradingPresenter implements ITradingPresenter {
 
     @Override
     public boolean closeStore(String sessionToken, String storeId) {
-        try {
-            Response<Boolean> response = storeService.closeStore(sessionToken, storeId);
-            if (response.getValue() != null && response.getValue()) {
-                // Get store name for the notification message
-                Response<StoreDTO> storeResponse = storePresenter.getStoreByName(sessionToken, storeId);
-                String storeName = storeResponse.errorOccurred() ? storeId : storeResponse.getValue().getName();
+        // try {
+        //     Response<Boolean> response = storeService.closeStore(sessionToken, storeId);
+        //     if (response.getValue() != null && response.getValue()) {
+        //         // Get store name for the notification message
+        //         Response<StoreDTO> storeResponse = storePresenter.getStoreByName(sessionToken, storeId);
+        //         String storeName = storeResponse.errorOccurred() ? storeId : storeResponse.getValue().getName();
                 
-                // Get all users with baskets in this store and notify them
-                Set<String> usersWithBaskets = storeService.getUsersWithBaskets(storeId);
-                for (String userId : usersWithBaskets) {
-                    webSocketNotifier.notifyUser(userId, 
-                        String.format("Store '%s' has been closed. Your shopping basket in this store has been cleared.", storeName));
-                }
-                return true;
-            }
-            return false;
-        } catch (Exception e) {
-            return false;
-        }
+        //         // Get all users with baskets in this store and notify them
+        //         Set<String> usersWithBaskets = storeService.getUsersWithBaskets(storeId);
+        //         for (String userId : usersWithBaskets) {
+        //             webSocketNotifier.notifyUser(userId, 
+        //                 String.format("Store '%s' has been closed. Your shopping basket in this store has been cleared.", storeName));
+        //         }
+        //         return true;
+        //     }
+        //     return false;
+        // } catch (Exception e) {
+        //     return false;
+        // }
+        return true; // Placeholder for actual implementation
     }
 
     @Override
